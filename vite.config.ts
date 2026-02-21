a4d9cf3da0f09304c2c047c9da1846c034d98aa0
nazki0325 <nazki0325@gmail.com>
2026-02-22 03:20:40 +0900

    typescript

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,19 @@ import { defineConfig, loadEnv } from 'vite';
 
 import fs from 'fs';
 
-export default({ mode }) => {
+export default(mode: string) => {
     process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
 
     return defineConfig({
         server: {
             host: true,
-            port: process.env.VITE_PORT,
+            port: Number(process.env.VITE_PORT),
             hmr: {
                 host: process.env.APP_HOST,
             },
             https: {
-                key: fs.readFileSync(process.env.VITE_HTTPS_KEY),
-                cert: fs.readFileSync(process.env.VITE_HTTPS_CERT),
+                key: fs.readFileSync(process.env.VITE_HTTPS_KEY!),
+                cert: fs.readFileSync(process.env.VITE_HTTPS_CERT!),
             },
             watch: {
                 usePolling: true,
