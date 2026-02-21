3f745f3e2ef37f0c741bac5b2f94bf574a824815
nazki0325 <nazki0325@gmail.com>
2026-02-21 22:50:56 +0900

    vite のポート

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,40 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default({ mode }) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
+
+    return defineConfig({
+        server: {
+            host: true,
+            port: 50173,
+            hmr: {
+                host: 'localhost',
             },
-        }),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
-});
+            watch: {
+                usePolling: true,
+            }
+        },
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            tailwindcss(),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+            wayfinder({
+                formVariants: true,
+            }),
+        ],
+    });
+}
